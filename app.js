6acd9e26e931d01558aab0fef33b19a53c9658ad
Mike Solomon <msolomon@thrillist.com>
2017-11-08 17:31:43 -0500

    log body when posting

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
+  console.log(req.body)
   switch(req.body.webhookEvent) {
     case 'comment_created':
       sendCommentNotification(req, res)
