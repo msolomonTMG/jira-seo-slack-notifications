37ab919fa41861b748335e3711d011fbd18e3c0e
Mike Solomon <msolomon@thrillist.com>
2017-11-08 17:14:12 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  slack = require('./slack'),
+  seoSlackChannel = process.env.SLACK_CHANNEL_SEO,
+  request = require('request');
+
+var app = express();
+app.set('port', process.env.PORT || 5000);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.post('/comment', function(req, res) {
+  let comment = req.body.comment,
+      issue = req.body.issue,
+      jiraURL = issue.self.split('/rest/api')[0];
+
+  let text = `${comment.author.displayName} commented on an issue`
+  let attachments = [
+    {
+      fallback: `${comment.author.displayName} commented on <${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
+      title: `<${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
+      thumb_url: `${comment.author.avatarUrls["48x48"]}`,
+      fields: [
+        {
+          title: "Comment",
+          value: `${comment.body}`,
+          short: false
+        }
+      ]
+    }
+  ]
+  slack.sendMessage([seoSlackChannel], text, attachments)
+})
+
+app.post('/created', function(req, res) {
+  let text,
+      color,
+      issue = req.body.issue,
+      jiraURL = issue.self.split('/rest/api')[0];
+
+  let attachments = [
+    {
+      fallback: `${issue.fields.creator.name} created <${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
+      title: `<${jiraURL}/browse/${issue.key}|${issue.key}: ${issue.fields.summary}>`,
+      thumb_url: `${issue.fields.creator.avatarUrls["48x48"]}`,
+      fields: [
+        {
+          title: "Description",
+          value: `${issue.fields.description}`,
+          short: false
+        }
+      ]
+    }
+  ]
+
+  let urls = [seoSlackChannel]
+  slack.sendMessage(urls, text, attachments)
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+module.exports = app;
