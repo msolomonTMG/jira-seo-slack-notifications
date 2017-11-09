b759e70d7c2771da750aa55e6efd8dee290d4365
Mike Solomon <msolomon@thrillist.com>
2017-11-09 13:20:01 -0500

    only send comment notification if comment was made

--- app.js
+++ app.js
@@ -19,10 +19,14 @@ app.post('/', function(req, res) {
   console.log(req.body)
   switch(req.body.webhookEvent) {
     case 'jira:issue_updated':
-      sendCommentNotification(req, res)
+      if (req.body.issue_event_type_name == 'issue_commented') {
+        sendCommentNotification(req, res)
+      } else {
+        res.sendStatus(200)
+      }
       break;
     case 'jira:issue_created':
-      sendIssueCreatedNotification(req, res) 
+      sendIssueCreatedNotification(req, res)
       break;
     default:
       res.sendStatus(200)
